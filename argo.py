a22d883f5563c52d57e997208d9797dbb336c3fc
Hossam Saraya <hossam.saraya@gmail.com>
2017-08-02 19:50:00 +0200

    Createdb command

--- argo.py
+++ argo.py
@@ -63,7 +63,7 @@ def db_dump():
 def db_restore():
     in_file = args.i or (args.e + '.dump')
     print("Restoring %s to local database argo_api_development..." % in_file)
-    os.system("dropdb argo_api_development && pg_restore -d argo_api_development %s" % in_file)
+    os.system("dropdb argo_api_development && createdb argo_api_development && pg_restore -d argo_api_development %s" % in_file)
     print('Done!')
 
 def db_sync():
